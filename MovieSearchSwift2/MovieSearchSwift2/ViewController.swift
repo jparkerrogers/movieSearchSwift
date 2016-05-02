6cd8076fec61fae0f0027578972c4a04a655bb8d
Jonathan Parker Rogers <parker.rogers24@gmail.com>
2016-05-01 22:51:00 -0600

    first commit

---
+++ MovieSearchSwift2/MovieSearchSwift2/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MovieSearchSwift2
+//
+//  Created by Jonathan Rogers on 5/1/16.
+//  Copyright © 2016 Jonathan Rogers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        // tower test
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
